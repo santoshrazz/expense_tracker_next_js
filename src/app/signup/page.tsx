af27301af513f5563cf8a7688e0c0dda9d4e565d
santoshrazz <santoshrajbgp11@gmail.com>
2024-07-03 18:28:28 +0530

    added new navbar and  introduced modal

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Signup } from "../Components/signup";
+
+const page = () => {
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+};
+
+export default page;
